0818d6693ddb52badd6084658bcd892d296a5f21
wakawaka54 <soue7@hotmail.com>
2016-07-01 00:54:57 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+WOAH.
+
+Aviato!
+
+From Silicon Valley.
+
+Will search Twitter Api and rank negativity of posts about certain airlines.
---
+++ src/Aviato/gulpfile.js
@@ -0,0 +1,56 @@
+﻿/// <binding Clean='clean' />
+"use strict";
+
+var gulp = require("gulp"),
+    rimraf = require("rimraf"),
+    concat = require("gulp-concat"),
+    cssmin = require("gulp-cssmin"),
+    uglify = require("gulp-uglify"),
+    less = require("gulp-less"),
+    path = require('path');
+
+var webroot = "./wwwroot/";
+
+var paths = {
+    js: webroot + "js/**/*.js",
+    minJs: webroot + "js/**/*.min.js",
+    css: webroot + "css/**/*.css",
+    minCss: webroot + "css/**/*.min.css",
+    concatJsDest: webroot + "js/site.min.js",
+    concatCssDest: webroot + "css/build/site.min.css",
+    less: webroot + "css/less/**/[^_]*.less",
+    lessDir: webroot + "css/less/",
+    lessDest: webroot + "css/",
+};
+
+gulp.task("clean:js", function (cb) {
+    rimraf(paths.concatJsDest, cb);
+});
+
+gulp.task("clean:css", function (cb) {
+    rimraf(paths.concatCssDest, cb);
+});
+
+gulp.task("clean", ["clean:js", "clean:css"]);
+
+gulp.task("min:js", function () {
+    return gulp.src([paths.js, "!" + paths.minJs], { base: "." })
+        .pipe(concat(paths.concatJsDest))
+        .pipe(uglify())
+        .pipe(gulp.dest("."));
+});
+
+gulp.task("min:css", function () {
+    return gulp.src([paths.css, "!" + paths.minCss])
+        .pipe(concat(paths.concatCssDest))
+        .pipe(cssmin())
+        .pipe(gulp.dest("."));
+});
+
+gulp.task("compile:less", function () {
+    return gulp.src(paths.less)
+        .pipe(less())
+        .pipe(gulp.dest(paths.lessDest))
+});
+
+gulp.task("min", ["min:js", "min:css"]);
